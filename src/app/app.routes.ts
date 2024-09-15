9f2892dbad7930abd967285a0413bd9edbf61b35
Damian Sire <damiansire@gmail.com>
2024-09-15 01:10:04 -0300

    feat: add routes and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FlightSelectorComponent } from '@pages/flight-selector/flight-selector.component';
+import { PriceDataListComponent } from '@pages/price-data-list/price-data-list.component';
+import { TravelPathCalculatorComponent } from '@pages/travel-path-calculator/travel-path-calculator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '**', redirectTo: 'all-tickets' },
+  { path: 'all-tickets', component: PriceDataListComponent },
+  { path: 'travel-path-calculator', component: TravelPathCalculatorComponent },
+  { path: 'flight-selector', component: FlightSelectorComponent },
+];
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { routes } from '../../../app.routes';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  routes = [
+    { path: 'all-tickets', name: 'All Tickets' },
+    {
+      path: 'travel-path-calculator',
+      name: 'Path Calculator',
+    },
+    { path: 'flight-selector', name: 'Flight Selector' },
+  ];
+}
